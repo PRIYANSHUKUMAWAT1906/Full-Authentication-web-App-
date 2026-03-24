bf9f38af734cd105278038aff0a331f71b6ba722
PRIYANSHUKUMAWAT1906 <er.priyanshu1906@gmail.com>
2026-03-24 14:21:06 +0530

    Adding mongoose library for conneting mondodb database to backend

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongnoose=require('mongoose');
+const connectDB= async()=>{
+try{
+    const conn=await mongnoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+}
+catch(error){
+    console.log(`Error:${error.message}`);
+    process.exit(1);
+}
+}
+module.exports=connectDB;
